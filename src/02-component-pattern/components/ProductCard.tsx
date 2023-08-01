f31f6402222e3189431d98fb9f0f658129ac4945
asepulvedaSMU <asepulveda@smu.cl>
2023-08-01 15:48:46 -0400

    first commit

---
+++ src/02-component-pattern/components/ProductCard.tsx
@@ -0,0 +1,32 @@
+import styles from '../styles/styles.module.css';
+import { useProduct } from '../hooks/useProduct';
+import { createContext } from 'react';
+import {
+  ProductCardProps,
+  ProductContextProps,
+} from '../interfaces/interfaces';
+import { ProductTitle } from './ProductTitle';
+import { ProductImage } from './ProductImage';
+import { ProductButtons } from './ProductButtons';
+
+export const ProductContext = createContext({} as ProductContextProps);
+const { Provider } = ProductContext;
+
+export const ProductCard = ({ product, children }: ProductCardProps) => {
+  const { counter, increaseBy } = useProduct();
+
+  return (
+    <Provider
+      value={{
+        counter,
+        increaseBy,
+        product,
+      }}
+    >
+      <div className={styles.productCard}>{children}</div>
+    </Provider>
+  );
+};
+ProductCard.Title = ProductTitle;
+ProductCard.Image = ProductImage;
+ProductCard.Buttons = ProductButtons;
