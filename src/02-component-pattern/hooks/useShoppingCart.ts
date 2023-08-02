e7a18d0df5e01105027786da3806a22f4bb987d6
asepulvedaSMU <asepulveda@smu.cl>
2023-08-02 15:37:26 -0400

    component control props alt

--- src/02-component-pattern/hooks/useShoppingCart.ts
+++ src/02-component-pattern/hooks/useShoppingCart.ts
@@ -1,4 +1,3 @@
-import { products } from '../data/product';
 import { Product, ProductInCart } from '../interfaces/interfaces';
 import { useState } from 'react';
 
@@ -15,31 +14,15 @@ export const useShoppingCart = () => {
     product: Product;
   }) => {
     setShoppingCart((oldShoppingCart) => {
-      const productInCart: ProductInCart = oldShoppingCart[product.id] || {
-        ...product,
-        count: 0,
-      };
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count;
-        return {
-          ...oldShoppingCart,
-          [product.id]: productInCart,
-        };
+      if (count === 0) {
+        const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+        return rest;
       }
-      const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-      return rest;
+      return {
+        ...oldShoppingCart,
+        [product.id]: { ...product, count },
+      };
     });
-
-    // setShoppingCart((oldShoppingCart) => {
-    //   if (count === 0) {
-    //     const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-    //     return rest;
-    //   }
-    //   return {
-    //     ...oldShoppingCart,
-    //     [product.id]: { ...product, count },
-    //   };
-    // });
   };
   return {
     shoppingCart,
